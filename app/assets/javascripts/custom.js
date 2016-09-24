80423aaf27b519315984cc0a58daefd657e2a234
Rahul Patil <rahulpatil2387@gmail.com>
2016-09-24 22:13:05 +0330

    Updates js folder to have radius layout js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,59 +1,81 @@
+/*
+	Radius by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
 jQuery(document).ready(function() {
-    // for hover dropdown menu
-    $('ul.nav li.dropdown').hover(function() {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
-    }, function() {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
-    });
-    // slick slider call 
-    $('.slick_slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slide: 'div',
-        autoplay: true,
-        autoplaySpeed: 2000,
-        cssEase: 'linear'
-    });
-    // slick slider2 call 
-    $('.slick_slider2').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        fade: true,
-        slide: 'div',
-        cssEase: 'linear'
-    });
-    //Check to see if the window is top if not then display button
-    jQuery(window).scroll(function() {
-        if ($(this).scrollTop() > 300) {
-            $('.scrollToTop').fadeIn();
-        } else {
-            $('.scrollToTop').fadeOut();
-        }
-    });
-    //Click event to scroll to top
-    $('.scrollToTop').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 800);
-        return false;
-    });
+(function($) {
+
+/*	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+*/
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$footer = $('#footer');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Header.
+			$header.each( function() {
+
+				var t 		= jQuery(this),
+					button 	= t.find('.button');
+
+				button.click(function(e) {
+
+					t.toggleClass('hide');
+
+					if ( t.hasClass('preview') ) {
+						return true;
+					} else {
+						e.preventDefault();
+					}
+
+				});
+
+			});
+
+		// Footer.
+			$footer.each( function() {
+
+				var t 		= jQuery(this),
+					inner 	= t.find('.inner'),
+					button 	= t.find('.info');
+
+				button.click(function(e) {
+					t.toggleClass('show');
+					e.preventDefault();
+				});
+
+			});
+
+	});
+
+})(jQuery);
 });
-
-// wow = new WOW({
-//     animateClass: 'animated',
-//     offset: 100
-// });
-// wow.init();
-
-jQuery(window).load(function() { // makes sure the whole site is loaded
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(100).css({
-        'overflow': 'visible'
-    });
-})
