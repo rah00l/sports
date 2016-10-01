c5db12841eced443119197bc8f5ed461a4797baa
Rahul Patil <rahulpatil2387@gmail.com>
2016-10-02 03:20:23 +0500

    Remove main css file not require

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,7 +3,7 @@
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
-jQuery(document).ready(function() {
+$(document).on('turbolinks:load', function() {
 (function($) {
 
 /*	skel.breakpoints({
