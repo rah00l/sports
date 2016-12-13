93924ad99e1ee8ed7c542607201ace52771061c2
Rahul <rahulpatil2387@gmail.com>
2016-12-14 04:49:37 +0530

    Add new getter and setter methods for equipment list and new task to collect all info

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -23,10 +23,168 @@ class Sport < ApplicationRecord
 	accepts_nested_attributes_for :players
 
 	def previous
-	  Sport.where("id < ?", self.id).last
+		Sport.where("id < ?", self.id).last
 	end
 
 	def next
-	  Sport.where("id > ?", self.id).first
+		Sport.where("id > ?", self.id).first
+	end
+
+	def equipment_list
+		equipment.map(&:name).join(", ")
+	end
+
+	def equipment_list=(items)
+		unless items.nil?
+			items = "cricket ball,cricket bat,stump, bail" if name.eql?('Cricket')
+			items = "Hockey puck, hockey stick, hockey skates" if name.eql?('Ice Hockey')
+			items = "Lacrosse stick, Lacrosse helmet, Lacrosse shoulder pads, Lacrosse elbow pads, Lacrosse gloves " if name.eql?('Lacrosse')
+			items = 'basketball' if name.eql?('SlamBall')
+			items = 'Poly' if name.eql?('table tennis ping pong')
+			items = 'flying disc' if name.eql?('Ultimate Frisbee')
+			items_string = items.include?(",") ? items.split(",") :  items.split("\n")
+			puts "**********************************************************"
+			puts items_string
+			puts "**********************************************************"
+			self.equipment = items_string.map do |n|
+				equipment_name = n.squish.downcase.tr(" ","_")
+				begin
+					puts '-----------------------------------------------------------------------'
+					puts equipment_name
+					puts '-----------------------------------------------------------------------'
+					wiki_name, extractor = '', ''
+					exceptional_soprts = ['Handball', 'Ice Hockey', 'Korfball', 'Netball','Padel', 
+						'Platform Tennis', 'Polo', 'Racquetball', 'Rounders',
+						'Rugby', 'Sepak Takraw', 'Softball', 'Squash',
+						'table tennis ping pong', 'Throwball', 'Tug of War']
+						wiki_name, extractor = if !exceptional_soprts.include?(name)
+							get_equipment_name(equipment_name)
+						end
+						p "080808080808080808080808080808080808"
+						puts wiki_name
+						puts extractor
+						p "080808080808080808080808080808080808"
+						doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{wiki_name}")) if wiki_name.present?
+			# if Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{equipment_name}")).present?
+			# 	doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{wiki_name}"))
+			# else
+			# 	wiki_name, extractor = get_equipment_name(name, equipment_name)
+			# 	doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{wiki_name}"))
+			# end
+		rescue => e
+			puts e
+		end
+		if doc.present?
+			desc = doc.css(extractor).text
+		else
+			puts "For #{equipment_name} -- #{wiki_name}-- and extractor is #{extractor}-- #{wiki_name} does not present"
+		end
+		Equipment.where(name: n.strip, description: desc).first_or_create!
+	end
+	end
+	end
+
+	def info_box_details=(info_box_hash)
+		unless info_box_hash.nil?
+			begin
+				InfoBox.where(info_box_hash).first_or_create!
+			rescue => e
+				puts e
+			end
+		end
+	end
+
+
+	def get_equipment_name(name)
+		equipment_name, extractor = case name
+		when 'football_(leather_prolate_spheroid)'
+			['Football_(ball)#American_and_Canadian_football', 'p:nth-child(14) , p:nth-child(15) , p:nth-child(13)']
+		when "pads_(shoulder_and_knee)"
+			""
+		when 'football'
+			['Football_(ball)#Australian_rules_football', 'p:nth-child(27) , p:nth-child(26) , .tleft+ p']
+		when 'shuttlecock'
+			['Shuttlecock', '.tright+ p']
+		when 'racquet'
+			['Racket_(sports_equipment)#Badminton', 'p:nth-child(17) , p:nth-child(16)']
+		when 'cue'
+			['Cue_stick', 'p:nth-child(4)']
+		when 'billiard_balls'
+			['Billiard_ball', 'p:nth-child(4)']
+		when 'skittles'
+			""
+		when 'baseball'
+			['Baseball_(ball)', 'p:nth-child(5) , .tright+ p']
+		when 'baseball_bat'
+			['Baseball_bat', 'p:nth-child(3)']
+		when 'baseball_glove'
+			['Baseball_glove', 'p:nth-child(5) , p:nth-child(4)']
+		when "bases"
+			""
+		when "basketball"
+			['Basketball_(ball)', 'p:nth-child(4) , p:nth-child(3)']
+		when 'billiard_ball'
+			['Billiard_ball', 'p:nth-child(4)']
+		when 'billiard_table'
+			['Billiard_table', 'p:nth-child(4)']
+		when 'bowl/wood_and_jack/kitty'
+			""
+		when 'stump'
+			['Stump_(cricket)', 'p:nth-child(12) , .tright+ p']
+		when 'bail'
+			['Bail_(cricket)', 'p:nth-child(7) , ul+ p , p:nth-child(2)']
+		when 'golf_clubs'
+			['Golf_club', 'p:nth-child(6) , p:nth-child(5)']
+		when 'and_others'
+			""
+		when 'appropriate_horse_tack'
+			['Horse_tack', 'p:nth-child(3)']
+		when 'lacrosse_stick'
+			['Lacrosse_stick', 'p:nth-child(2)']
+		when 'lacrosse_shoulder_pads'
+			''
+		when 'lacrosse_elbow_pads'
+			""
+		when 'wiffle_ball'
+			['Wiffle_ball', '.tright+ p']
+		when 'pickleball_paddle'
+			""
+		when 'roller_skates'
+			['Roller_skates', 'p:nth-child(12) , .tright+ p']
+		when 'helmet'
+			['Helmet', 'p:nth-child(7) , p:nth-child(6) , p:nth-child(5)']
+		when 'knee_pads'
+			['Knee_pad', '.tright+ p']
+		when 'elbow_pads'
+			['Elbow_pad', 'p']
+		when 'wrist_guards'
+			['Wrist_guard', 'p:nth-child(6) , .tright+ p']
+		when 'mouthguard'
+			['Mouthguard', 'p:nth-child(4)']
+		when 'wilson_custom_–_all-white_"wave"_basketball'
+			""
+		when 'snooker_balls'
+			['Billiard_ball#Snooker', 'p:nth-child(40) , p:nth-child(39) , .wikitable+ p']
+		when 'tennis_racket'
+			['Racket_(sports_equipment)#Tennis', 'p:nth-child(38) , p:nth-child(39)']
+		when 'diving_mask'
+			['Diving_mask', '.infobox+ p']
+		when 'snorkel'
+			['Snorkeling#The_snorkel', 'p:nth-child(7) , p:nth-child(6) , p:nth-child(5)']
+		when 'fins'
+			""
+		when 'water_polo_cap'
+			""
+		when 'stick_&_puck.'
+			""
+		when 'volleyball'
+			['Volleyball_(ball)', '.tright+ p']
+		when 'water_polo_goals'
+			['Water_polo#Water_polo_equipment', '.tright+ ul li , ul:nth-child(70) li , .tmulti~ .hatnote+ p']
+		when 'water_polo_caps'
+			['Water_polo_cap', 'p:nth-child(2)']
+		else
+			[name, 'p:nth-child(4) , p:nth-child(3)']
+		end
 	end
 end
---
+++ lib/tasks/extract_and_feed_sports_sport_metadata.rake
@@ -0,0 +1,70 @@
+# Extract and feed sports metadata
+namespace :sports do
+	desc "Extract and feed sports metadata"
+	task :extract_and_feed_metadata => :environment do
+		# Sport.all.each do |sport|
+				# sport_name = 'aussie rules football' # sport_name = 'badminton' # sport_name = "bar billiards" 
+				#sport_name = "baseball"sport_name = 'basketball' sport_name = "American Football"
+				# sport_name = "archery" sport_name = "arm wrestling"
+				# sport_name = "bar billiards"
+				sport_name = "wiffle ball"
+				wiki_sport = get_sport_name_wiki(sport_name)
+				wiki_sport = sport_name.downcase.tr(' ', '_') unless (sport_name.eql?('Padel') || sport_name.eql?('Squash') || sport_name.eql?('table tennis ping pong'))
+				doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{wiki_sport}"))
+				table = doc.css('table.vcard').first
+				if table.present?
+					rows = table.css('tr')
+					column_names = rows.shift.css('th').map(&:text)
+						text_all_rows = rows.map do |row|
+							row_name = row.css('th').text
+							row_values = row.css('td').map(&:text)
+							[row_name, *row_values]
+						end
+						p column_names
+						p text_all_rows
+						all_rows = []
+
+						text_all_rows.each {|row| all_rows << row if row.size == 2}
+						all_rows_hash = Hash[*all_rows.flatten!]
+						new_all_rows_hash = {}
+
+						all_rows_hash.to_hash.each_pair { |k,v| new_all_rows_hash.merge!({k.squish.downcase.tr(" ","_") => v}) }
+						puts "......."*7
+
+						puts new_all_rows_hash.symbolize_keys
+
+						if new_all_rows_hash.respond_to?(:keys)
+							sport = Sport.find_by(name: sport_name)
+							info_box_hash = new_all_rows_hash.symbolize_keys.except(:nicknames, :registered_players, :clubs, 
+														:contact, :type, :equipment, :venue, :country_or_region, :paralympic, :created)
+							sport.info_box.destroy if sport.info_box.present?
+							sport.info_box_details=(info_box_hash.merge!({sport_id: sport.id}))
+
+							sport.equipment.destroy_all if sport.equipment.present?
+							sport.equipment_list=(new_all_rows_hash.symbolize_keys[:equipment]) if sport.present?
+						end
+				else 
+					puts "No vcard found for '#{sport_name}'"
+				end
+			# end
+		end
+	end
+
+def get_sport_name_wiki(sport)
+	sport_name = case sport
+	when 'Kin-Ball'
+		'kin-Ball'
+	when 'mma mixed martial arts'
+		'Mixed_martial_arts'
+	when 'Padel'
+		'Padel_(sport)'
+	when 'Squash'
+		'Squash_(sport)'
+	when 'Ultimate Frisbee'
+		'Ultimate_(sport)'
+	when 'table tennis ping pong'
+		'Table_tennis'
+	else
+		sport
+	end
+end
